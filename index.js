f224a16198af26a9a3b86d391b4cda77a806841e
kunal swami <kunalswami29@gmail.com>
2020-05-23 19:42:24 +0530

    first commit:express server is fired

---
+++ index.js
@@ -0,0 +1,27 @@
+// to access express library
+const express=require('express');
+
+//to use express
+const app=express();
+
+//to set the port 
+const port =8000;
+
+
+
+
+
+
+
+
+
+
+
+
+//to check the status of server or telling express what to after setting server
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in setting up the server:${err}`); //interpolation used
+    };
+    console.log(`server is running up on port:${port}`); // interpolation used
+});
